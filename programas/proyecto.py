2851093ac90d19e7e5daf1dbf1c8803648c0d052
CloudzV3 <martin.tin0042@gmail.com>
2023-06-06 14:45:11 -0600

    se agregan funciones de promedio, mediana y moda. Falta poner lectura con csv (de 4 columnas)

--- programas/proyecto.py
+++ programas/proyecto.py
@@ -1,14 +1,29 @@
-
+import statistics as st
 
 #lectura de archivo rango.txt
-try:
-    archivo = open('rango.txt', 'r+')
-    rango = [linea.strip() for linea in archivo.readlines()]
+def read_and_load_data():
+    try:
+        archivo = open('datos.txt', 'r') 
+        datos = [float(linea.strip()) for linea in archivo.readlines()]  
+    except FileNotFoundError:
+        print("El archivo no existe. Checa que esté en el directorio jaja")
+    except IOError:
+        print("No se pudo leer el archivo :(( pásame unos lentes ajaj")    
+    finally: 
+        archivo.close()
+    return datos  
 
-    print(rango)
-except FileNotFoundError:
-    print("El archivo no existe. Checa que esté en el directorio jaja")
-except IOError:
-    print("No se pudo leer el archivo :(( pásame unos lentes ajaj")    
-finally: 
-    archivo.close()
+data = read_and_load_data()
+
+def mean(datos):
+    print('\t\tPROMEDIO\nEl promedio de los datos ingresados es de: ',round((sum(datos) / len(datos)),2))
+
+def median(datos):
+    print('\t\tMEDIANA\nLa mediana de los datos ingresados es de: ', (len(datos) + 1) / 2)
+
+def mode(datos):
+    print('\t\tMODA\nLa moda de los datos ingresados es de: ', st.mode(datos))
+
+mean(data)
+median(data)
+mode(data)
