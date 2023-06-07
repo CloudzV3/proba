0b54926a12aef1d56e4f4fd7b9687c5546aa32a8
CloudzV3 <martin.tin0042@gmail.com>
2023-06-07 14:58:25 -0600

    Se agrega función de intervalos y desviación estándar

--- programas/proyecto.py
+++ programas/proyecto.py
@@ -1,5 +1,5 @@
 import statistics as st
-
+import math
 #lectura de archivo rango.txt
 def read_and_load_data():
     try:
@@ -13,26 +13,29 @@ def read_and_load_data():
         archivo.close()
     return datos
 
+#impresión de datos
 def print_data(datos):  
     print("\t\tDATOS ORDENADOS\nLos datos son : ", datos)
     print('==========================================================================')
 
+#promedio e impresión del mismo también regresa el valor
 def mean(datos):
     promedio = round((sum(datos) / len(datos)),2)
     print('\t\tPROMEDIO\nEl promedio de los datos ingresados es de: ', promedio)
     print('==========================================================================')
     return promedio
-
+#mediana e impresión del mismo
 def median(datos):
     mediana = ((len(datos) + 1) // 2)
     print('\t\tMEDIANA\nLa mediana se encuentra en la posición: ', mediana)
     print('El dato en dicha posición es: ',datos[mediana - 1])  #mediana - 1 por q la lista empieza desde 
     print('==========================================================================')
-
+#moda e impresión del mismo
 def mode(datos):
     print('\t\tMODA\nLa moda de los datos ingresados es de: ', st.mode(datos))
     print('==========================================================================')
 
+#cuartiles e impresión del mismo
 def quartiles(datos):
     print('\t\tCUARTILES')
     for i in range(1,4):
@@ -40,14 +43,18 @@ def quartiles(datos):
         print('La posición del cuartil Q' , i , ' es: ', cuartil)
         print('Dicha posición del cuartil Q' , i , ' tiene el valor de: ' , datos[cuartil - 1]) #cuartil - 1 por q la lista empieza desde cero
     print('==========================================================================')
-        
-def variance(datos,promedio):
-    print('\t\tVARIANZA')
+
+#varianza e impresión del mismo        
+def variance_and_standard_deviation(datos,promedio):
+    print('\t\tVARIANZA Y DESVIACIÓN ESTÁNDAR')
     sumatoria = [round(((dato - promedio) ** 2),2) for dato in datos]
-    varianza = sum(sumatoria) / len(datos)
+    varianza = sum(sumatoria) / len(datos)  #si se requiere la fórmula de muestra es n - 1 en vez de n en el denominador
     print('La varianza (suponiendo que es una muestra) es de: ', round(varianza,2))
+    print('La desviación estándar es: ', round((math.sqrt(round(varianza,2))),2))
     print('==========================================================================')
 
+
+#geenrar diagrama de tallo hojas
 def diagrama_tallo_hojas(data):
     tallos = []
     hojas = []
@@ -73,16 +80,44 @@ def diagrama_tallo_hojas(data):
         print()
     print('==========================================================================')
 
+def intervals (datos):
+    print('\t\tINTERVALOS')
+    minimo = min(datos)
+    maximo = max(datos)
+    rango = maximo - minimo
+    k_intervalos = 1 + 3.322*math.log10(len(datos))
+    if(math.floor(k_intervalos) % 2 != 0):  #se recomienda redondear a abajo, pero si es par ese número
+        pass                                #-> se redondea a arriba
+    else:
+        k_intervalos = math.ceil(k_intervalos)
+
+    print('número de intervalos: ', k_intervalos)
+    amplitud = round(rango / k_intervalos)
+    intervalos = []
+    inicio = 0
+
+    for i in range(k_intervalos - 1):
+        if(i == 0):
+            inicio = minimo + amplitud
+            intervalo = [minimo, inicio]
+            intervalos.append(intervalo)
+        inicio = inicio + amplitud
+        intervalo = [inicio - amplitud, inicio]
+        intervalos.append(intervalo)
+    
+    for intervalo in intervalos:
+        print(intervalo)
+    print('==========================================================================')
 
 
 
 data = read_and_load_data()
 data.sort()
-
-print_data(data)
-mean(data)  
+                    # llamada a funciones
+print_data(data)  
 median(data) #revisar si puede colocar un lugar para números pares (o sea que se coloque entre los dos números donde debería de estar)
 mode(data)
+variance_and_standard_deviation(data,mean(data))
 quartiles(data) #revisar si puede colocar un lugar para números pares (o sea que se coloque entre los dos números donde debería de estar)
-variance(data,mean(data))
 diagrama_tallo_hojas(data)
+intervals(data)
